956931e06afbd1dd524b03aec39e0b7d076cf1e1
liusonglei <2123680589@qq.com>
2025-08-03 19:39:49 +0800

    Site updated: 2025-08-03 19:39:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/02/hello-world/","2025/08/02/第一篇/"];function toRandomPost(){
+var posts=["2025/08/03/hello-world/","2025/08/02/第一篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
