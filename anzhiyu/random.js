038b1e5cb78d231e0aabd23745286b85ff5664ec
liusonglei <2123680589@qq.com>
2025-08-14 14:38:59 +0800

    Site updated: 2025-08-14 14:38:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/13/Arch Linux 虚拟机安装指南/"];function toRandomPost(){
+var posts=["2025/01/14/Arch Linux 从终端到桌面/","2025/01/13/Arch Linux 虚拟机安装指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
