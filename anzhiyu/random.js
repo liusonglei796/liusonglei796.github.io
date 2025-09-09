3375324ec5087c33cc543c25d6bb4dddbc73653c
liusonglei <2123680589@qq.com>
2025-09-09 11:29:13 +0800

    Site updated: 2025-09-09 11:29:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/08/vmware/","2025/01/14/一些个人认为有用的资源/","2025/01/13/Arch Linux 虚拟机安装指南/"];function toRandomPost(){
+var posts=["2025/01/13/Arch Linux 虚拟机安装指南/","2025/01/14/一些个人认为有用的资源/","2025/09/08/vmware/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
