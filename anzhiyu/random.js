d90cd24b8592ff5f2b3f45ea267cc56d9e020d7e
Lay <2123680589@qq.com>
2026-03-01 11:21:13 +0800

    Site updated: 2026-03-01 11:21:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/14/new-article/","2025/09/08/vmware/","2025/09/14/新文章/","2025/01/13/Arch Linux 虚拟机安装指南/"];function toRandomPost(){
+var posts=["2025/09/14/new-article/","2025/01/13/Arch Linux 虚拟机安装指南/","2026/03/01/大模型会不会取代程序员/","2025/09/08/vmware/","2025/09/14/新文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
