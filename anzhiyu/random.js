f764e8b654556323e0352ff98ca24146ab6f63b8
liusonglei796 <liusonglei796@gmail.com>
2026-06-19 18:30:00 +0800

    Site updated: 2026-06-19 18:29:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/10/1Go 语言后台定时任务：从“事故频发”到“工业级稳健”的设计指南/","2025/01/13/Arch Linux 虚拟机安装指南/","2026/04/02/Cloudflare Tunnel 配置指南/","2026/03/01/DeepSeek关于编程与AI的QA/","2026/03/19/Go语言实习面试问题复盘/","2025/09/08/vmware/","2025/09/14/new-article/","2025/09/14/新文章/","2026/05/30/OpenCode教学文档/"];function toRandomPost(){
+var posts=["2026/04/10/1Go 语言后台定时任务：从“事故频发”到“工业级稳健”的设计指南/","2026/04/02/Cloudflare Tunnel 配置指南/","2026/03/01/DeepSeek关于编程与AI的QA/","2026/05/30/OpenCode教学文档/","2026/03/19/Go语言实习面试问题复盘/","2025/01/13/Arch Linux 虚拟机安装指南/","2025/09/08/vmware/","2025/09/14/new-article/","2026/06/19/SOLID原则笔记-以DIP为主线/","2025/09/14/新文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
